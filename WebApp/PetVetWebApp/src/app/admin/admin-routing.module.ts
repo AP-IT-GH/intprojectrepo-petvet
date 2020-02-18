1bff2db813485d1bad190f784cccfaf0d6151b9e
Cedric Van Opstal <cedric.vanopstal@student.ap.be>
2020-02-18 15:31:14 +0100

    Firebase Auth Part 1

---
+++ WebApp/PetVetWebApp/src/app/admin/admin-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from  './login/login.component';
+import { RegisterComponent } from  './register/register.component';
+import { ForgotPasswordComponent } from  './forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from  './verify-email/verify-email.component';
+/*
+const  routes:  Routes  = [
+{
+path:  'admin',
+component:  ProjectComponent,
+
+
+children: [
+    // [...]
+    { path:  'login',component:  LoginComponent},
+    { path:  'register', component:  RegisterComponent },
+    { path:  'forgot-password', component:  ForgotPasswordComponent },
+    { path:  'verify-email', component:  VerifyEmailComponent }
+]
+}
+];
+*/
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
--- WebApp/PetVetWebApp/src/app/app.module.ts
+++ WebApp/PetVetWebApp/src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AngularFireModule} from "@angular/fire";
+import {AngularFireAuthModule} from "@angular/fire/auth";
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './admin/login/login.component';
+import { RegisterComponent } from './admin/register/register.component';
+import { ForgotPasswordComponent } from './admin/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './admin/verify-email/verify-email.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-EWtCcadCjtv9zX9x-thILOlBh-MXPd4",
+  authDomain: "petvet-268116.firebaseapp.com",
+  databaseURL: "https://petvet-268116.firebaseio.com/",
+  projectId: "petvet-268116",
+  storageBucket: "petvet-268116.appspot.com",
+  messagingSenderId: "1080934441565",
+  appId: "1:1080934441565:web:51fbda4495c98722bd23b4",
+  measurementId: "G-XZ28FNEXX1"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
