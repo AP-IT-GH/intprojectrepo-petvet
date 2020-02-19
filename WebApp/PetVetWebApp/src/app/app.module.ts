407388c267479d64f5a7c71ba14bb4cc17a92fb7
Cedric Van Opstal <cedric.vanopstal@student.ap.be>
2020-02-19 09:37:56 +0100

    api key weg

--- WebApp/PetVetWebApp/src/app/app.module.ts
+++ WebApp/PetVetWebApp/src/app/app.module.ts
@@ -11,14 +11,7 @@ import { ForgotPasswordComponent } from './admin/forgot-password/forgot-password
 import { VerifyEmailComponent } from './admin/verify-email/verify-email.component';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-EWtCcadCjtv9zX9x-thILOlBh-MXPd4",
-  authDomain: "petvet-268116.firebaseapp.com",
-  databaseURL: "https://petvet-268116.firebaseio.com/",
-  projectId: "petvet-268116",
-  storageBucket: "petvet-268116.appspot.com",
-  messagingSenderId: "1080934441565",
-  appId: "1:1080934441565:web:51fbda4495c98722bd23b4",
-  measurementId: "G-XZ28FNEXX1"
+  //copy paste api key
 };
 
 @NgModule({
