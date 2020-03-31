5f0db9a5ae4f80bb63455bdfa903d6f5cb466876
Niels <niels.verschooten@student.ap.be>
2020-04-01 01:24:26 +0200

    addpetcode added (not ready)

---
+++ AndroidApp/app/src/main/java/com/internationalproject/petvet/AddPetActivity.java
@@ -0,0 +1,13 @@
+package com.internationalproject.petvet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_pet);
+    }
+}
