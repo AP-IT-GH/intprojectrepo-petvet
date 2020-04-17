ba7079ce87fa82bc45b5cdd893f50b56c228f207
Tim <s110032@ap.be>
2020-04-17 13:47:30 +0200

    beginscherm

--- AndroidApp/app/src/main/java/com/internationalproject/petvet/MainActivity.java
+++ AndroidApp/app/src/main/java/com/internationalproject/petvet/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent i = new Intent(this, MyPetsActivity.class);
+    }
+
+    public void StartPV(View view) {
+        Intent i = new Intent(this, MenuActivity.class);
         startActivity(i);
     }
+
 }
